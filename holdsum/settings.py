b7651a97046289b3dfb74061ed14048cffd35142
amelius15 <anthonylobko@gmail.com>
2016-03-04 03:43:16 -0500

    fixed SITE_ID

--- holdsum/settings.py
+++ holdsum/settings.py
@@ -121,7 +121,8 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+SITE_ID = 1
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 import requests
-access_token = 'CAAXzctM9yiEBAJmOztKaToBr54FqaZA6OnhUjv5cuRg5sphoB9hJVEdFjPPO6NeuLMZBLZCQ7JM5ZBylJy0CWy4YBfwjlcPfjKhGyqWLYEyZBR2CTQ1S0AvesS13BwadvZATMC78z6ykzJMX96ICnbiLc5z3jUbKjsfbaBSDvN2WgrbboVCz7ZB39aWNkKCZCj0MmU62xCI2ZBgZDZD'
-r = requests.post('https://holdsum.herokuapp.com/auth/facebook/', json = {'access_token':access_token})
+username = 'test'
+p1 = 'passwordprime'
+p2 = p1
+email = "bla@gmail.bla"
+r = requests.post('https://holdsum.herokuapp.com/auth/registration/'
+    , json = {'username':username, 'password1': p1, 
+    'password2': p2, 'email':email
+    })
 print (r.text)
