a085b32b40bd2b8a53406919f365b08ef438098f
Josh Schneier <josh.schneier@gmail.com>
2016-08-27 18:21:41 -0400

    Set the yodleepw and save the instance

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,8 +1,10 @@
-from .yodlee import yodlee
+from .yodlee import yodlee, gen_password
 
 
 def on_create_link_yodlee(created=False, instance=None, **kwargs):
     if not created:
         return
 
+    instance.yodleepw = gen_password()
+    instance.save()
     yodlee.register3(instance)
