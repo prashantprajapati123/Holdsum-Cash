56d70858bded1102b3ebab91297c2a525d7df28d
Josh Schneier <josh.schneier@gmail.com>
2016-09-15 23:18:40 -0400

    Add the LoanRequest model

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,8 +1,10 @@
+from decimal import Decimal as D
 from django.contrib import admin
 
 import nested_admin
 
-from .models import Choice, Question, Questionnaire
+from .models import Choice, LoanRequest, Question, Questionnaire,\
+    QuestionResponse
 
 
 class ChoiceInline(nested_admin.NestedTabularInline):
@@ -16,6 +18,7 @@ class QuestionInline(nested_admin.NestedTabularInline):
     extra = 0
 
 
+@admin.register(Questionnaire)
 class QuestionnaireAdmin(nested_admin.NestedModelAdmin):
     inlines = [QuestionInline]
 
@@ -23,4 +26,40 @@ class QuestionnaireAdmin(nested_admin.NestedModelAdmin):
         return False
 
 
-admin.site.register(Questionnaire, QuestionnaireAdmin)
+class QuestionResponseInline(admin.TabularInline):
+    model = QuestionResponse
+    fields = ('question', 'answer', 'textbox', 'weight', 'score', 'total',)
+    readonly_fields = ('question', 'answer', 'textbox', 'weight', 'score', 'total')
+
+    def question(self, obj):
+        return obj.choice.question.query
+
+    def answer(self, obj):
+        return obj.choice.option
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(LoanRequest)
+class LoanRequestAdmin(admin.ModelAdmin):
+    inlines = [QuestionResponseInline]
+    list_display = (
+        'borrower',
+        'amount',
+        'state',
+    )
+
+    readonly_fields = [
+        'borrower',
+        'amount',
+        'state',
+        'score',
+    ]
+
+    def score(self, obj):
+        return '%g / 100' % obj.total_questions_score
+    score.short_description = 'Total Score'
--- transactions/models.py
+++ transactions/models.py
@@ -21,7 +21,7 @@ class Questionnaire(models.Model):
 
 
 class Question(TimeStampedModel):
-    questionnaire = models.ForeignKey(Questionnaire, models.PROTECT)
+    questionnaire = models.ForeignKey('transactions.Questionnaire', models.PROTECT)
     query = models.CharField(max_length=150)
     score = models.DecimalField(max_digits=4, decimal_places=2, help_text='e.g. 30 for 30%')
     alt_text = models.CharField(max_length=100, blank=True, null=True)
@@ -31,7 +31,7 @@ class Question(TimeStampedModel):
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, models.CASCADE, related_name='choices')
+    question = models.ForeignKey('transactions.Question', models.CASCADE, related_name='choices')
     option = models.CharField(max_length=100)
     weight = models.DecimalField(max_digits=4, decimal_places=2, help_text='0 - 5 for weighting the score.',
                                  validators=[MinValueValidator(D(0), MaxValueValidator(D(5)))])
@@ -40,3 +40,41 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.option
+
+
+class QuestionResponse(models.Model):
+    request = models.ForeignKey('transactions.LoanRequest',
+                                related_name='responses')
+    choice = models.ForeignKey(Choice, models.PROTECT)
+    textbox = models.TextField(blank=True, null=True)
+
+    # these are saved from the choice/question
+    score = models.DecimalField(max_digits=4, decimal_places=2)
+    weight = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return 'Response to Question #{}'.format(self.choice.question_id)
+
+    def save(self, **kwargs):
+        if not self.score:
+            self.score = self.choice.question.score
+        if not self.weight:
+            self.weight = self.choice.question.weight
+        super().save(**kwargs)
+
+    @property
+    def total(self):
+        return (self.weight / D(5)) * self.score if self.id else None
+
+
+class LoanRequest(TimeStampedModel):
+    borrower = models.ForeignKey('accounts.Profile', models.PROTECT)
+    amount = models.DecimalField(decimal_places=2, max_digits=7)
+    state = models.CharField(max_length=30, choices=LOAN_STATES, default=LOAN_STATES.pending)
+
+    def __str__(self):
+        return '$%g request from %s' % (self.amount, self.borrower.user.username)
+
+    @property
+    def total_questions_score(self):
+        return sum(r.total for r in self.responses.all())
