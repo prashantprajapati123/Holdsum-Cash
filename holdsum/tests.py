ba63a981fa85d130480723f797e41d692db70fad
Josh Schneier <josh.schneier@gmail.com>
2016-10-01 16:23:13 -0400

    Add some basic tests for the loan request creation

---
+++ holdsum/tests.py
@@ -0,0 +1,27 @@
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+from rest_framework.authtoken.models import Token
+
+
+class UserAPITestCase(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.key = self.create_user()
+        self.user = Token.objects.get(key=self.key).user
+
+    def create_user(self):
+        url = reverse('rest_register')
+        data = dict(username='MrSmith', email='mrsmith@gmail.com',
+                    password1='loantom3', password2='loantom3')
+        return self.client.post(url, data).data['key']
+
+    def auth_get(self, url, **kwargs):
+        return self.client.get(url,
+                               HTTP_AUTHORIZATION='Token %s' % self.key,
+                               **kwargs)
+
+    def auth_post(self, url, data, **kwargs):
+        return self.client.post(url, data,
+                                HTTP_AUTHORIZATION='Token %s' % self.key,
+                                **kwargs)
--- transactions/tests.py
+++ transactions/tests.py
@@ -1,9 +1,12 @@
 from decimal import Decimal as D
 from django.core.exceptions import ValidationError
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 from model_mommy import mommy
+from rest_framework.test import APIRequestFactory
 
+from holdsum.tests import UserAPITestCase
 from .models import Choice, LoanRequest, Question, QuestionResponse
 
 
@@ -52,5 +55,16 @@ class QuestionResponseTests(TestCase):
         self.assertEqual(qr.total, D(20))
 
 
-class LoanRequestTests(TestCase):
-    pass
+class LoanRequestAPITests(UserAPITestCase):
+    def setUp(self):
+        super().setUp()
+        self.url = reverse('loan_request-list')
+        self.factory = APIRequestFactory()
+
+    def test_requires_authentication(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 401)
+
+    def test_requires_post(self):
+        response = self.auth_get(self.url)
+        self.assertEqual(response.status_code, 405)
