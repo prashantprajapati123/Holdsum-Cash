bfb517aea06bda74216ad25caea2346331a11ba4
Josh Schneier <josh.schneier@gmail.com>
2016-10-05 02:48:35 -0400

    Update the admin to show the rest of the data

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
-from .models import User
+from .models import Employment, User
+
+
+class EmploymentInline(admin.StackedInline):
+    model = Employment
 
 
 @admin.register(User)
 class UserAdmin(DjangoUserAdmin):
-    pass
+    inlines = [EmploymentInline]
+
+    fieldsets = (
+        (None,
+            {'fields': ('username', 'password', 'status')}),
+        ('Personal info',
+            {'fields': ('first_name', 'last_name', 'email', 'sex')}),
+        ('Address',
+            {'fields': ('address', 'city', 'state', 'zip_code'),
+             'classes': ('collapse',)}),
+        ('Documents',
+            {'fields': ('license', 'paystubs'),
+             'classes': ('collapse',)}),
+    )
