8fceab2938ec82dce5b301428a16196f9d911bce
Josh Schneier <josh.schneier@gmail.com>
2016-10-24 01:29:55 -0400

    Only allow approved users to make loan requests

--- transactions/views.py
+++ transactions/views.py
@@ -1,5 +1,7 @@
+from rest_framework.exceptions import PermissionDenied
 from rest_framework import decorators, mixins, permissions, response, viewsets
 
+from accounts.models import STATUS_CHOICES
 from . import docusign
 from .models import LoanRequest
 from .serializers import LoanRequestSerializer
@@ -13,6 +15,11 @@ class LoanRequestViewSet(mixins.CreateModelMixin,
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = LoanRequestSerializer
     queryset = LoanRequest.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        if request.user.status != STATUS_CHOICES.approved:
+            raise PermissionDenied
+        return super().create(request, *args, **kwargs)
 
     @decorators.detail_route(methods=['POST'], url_path='signing-url')
     def signing_url(self, request, pk=None):
