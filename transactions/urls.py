f956f131ae7362206243775eb5f642e7e1ffe8dc
aditya.r <adityaraaj1823@gmail.com>
2018-03-30 02:06:31 +0530

    new api added for socre calculation

--- transactions/urls.py
+++ transactions/urls.py
@@ -2,13 +2,14 @@ from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
 
 from .views import LoanRequestViewSet
-from .views import UserLoanRequestsViewSet, QuestionResponseViewSet, GetUserQuestionResponseList
+from .views import UserLoanRequestsViewSet, QuestionResponseViewSet, GetUserQuestionResponseList,GetUserScoreDetails
 
 router = DefaultRouter()
 router.register(r'loan-request', LoanRequestViewSet, base_name='loan_request')
 router.register(r'user-loan-request', UserLoanRequestsViewSet, base_name='user_loan_request')
 router.register(r'save-questionnaire-choice', QuestionResponseViewSet, base_name='save_questionnaire_choice')
 router.register(r'get-questionnaire-choice/(?P<request_id>\w+)', GetUserQuestionResponseList, base_name='get_questionnaire_choice')
+router.register(r'get-user-score', GetUserScoreDetails, base_name='get_user_score')
 
 
 
@@ -16,4 +17,6 @@ router.register(r'get-questionnaire-choice/(?P<request_id>\w+)', GetUserQuestion
 
 urlpatterns = [
     url('', include(router.urls)),
+    url(r'get-user-score/$', GetUserScoreDetails.as_view(), name='get_user_score'),
+
 ]
--- transactions/views.py
+++ transactions/views.py
@@ -3,8 +3,24 @@ from rest_framework import decorators, mixins, permissions, response, viewsets
 
 from accounts.models import STATUS_CHOICES
 from . import docusign
-from .models import LoanRequest,QuestionResponse 
+from .models import LoanRequest,QuestionResponse,Choice 
 from .serializers import LoanRequestSerializer,QuestionResponseSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from django.conf import settings
+from accounts.plaidclient import Client
+from plaid.errors import PlaidError
+from decimal import Decimal as D
+from rest_framework.response import Response
+from .signals import get_plaid_modifier
+import datetime
+import logging
+log = logging.getLogger('plaid')
+
+
+
+
+
 
 
 class LoanRequestViewSet(mixins.ListModelMixin, mixins.CreateModelMixin,
@@ -76,3 +92,68 @@ class GetUserQuestionResponseList(mixins.ListModelMixin,mixins.CreateModelMixin,
         request_id = self.kwargs['request_id']
         return QuestionResponse.objects.filter(request=request_id)
     
+
+
+
+class GetUserScoreDetails(APIView):
+    #permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        public_token = request.data['plaidToken']
+        client = Client(client_id=settings.PLAID_CLIENT_ID, secret=settings.PLAID_SECRET, public_key=settings.PLAID_PUBLIC_KEY, environment='sandbox')
+        total = 0 
+        try:
+            response = client.Item.public_token.exchange(public_token)
+            request.user.plaid_access_token = response['access_token']
+            request.user.plaid_public_token = public_token
+            request.user.save()
+
+            choice_resposne = request.data['responses']
+
+            for ch in choice_resposne:
+                try:
+                    ch_obj = Choice.objects.get(id=ch['choice'])
+                except:
+                    pass
+                if ch_obj:
+                    score = (ch_obj.weight / D(5)) * ch_obj.question.score
+                else:
+                    score = 0
+                total = total + score
+            start_date = "{:%Y-%m-%d}".format(datetime.datetime.now() + datetime.timedelta(-30))
+            end_date = "{:%Y-%m-%d}".format(datetime.datetime.now())
+            response = client.Transactions.get(request.user.plaid_access_token, start_date=start_date, end_date=end_date)
+            transactions = response['transactions']
+            user_plaid_score = get_plaid_modifier(transactions)
+
+            return Response({'status':self.get_status(total,user_plaid_score)})
+        except PlaidError as e:
+            log.warning('Issue with Plaid! Code %s, Message: %s', e.code, e.message)
+            return Response({'error': 'Something went wrong.'},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
+
+    def get_status(self,total,user_plaid_score):
+        current_score = per = 0
+        if user_plaid_score == 15:
+            per = (total/100) * 15 
+            current_score = per + total
+        elif user_plaid_score == 12:
+            per = (total/100) * 7.5
+            current_score = total - per
+        elif user_plaid_score == 9:
+            per = (total/100) * 15
+            current_score = total - per
+        elif user_plaid_score == -9:
+            per = (total/100) * 22.5
+            current_score = total - per
+        elif user_plaid_score == -12:
+            per = (total/100) * 22.5
+            current_score = total - per
+
+
+        if current_score >= 60:
+            return'approved'
+        else:
+            return 'declined'
